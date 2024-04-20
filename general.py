b5e0ff969626ffefd8f05bc4e4554a9cfc8b61a0
Tobie <79232946+tobie02@users.noreply.github.com>
2024-04-20 02:12:12 -0300

    Add files via upload

---
+++ general.py
@@ -0,0 +1,111 @@
+import discord
+from discord.ext import commands
+from gtts import gTTS
+import requests
+
+class General(commands.Cog, name='General'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help='Muestra la info del bot')
+    async def info(self, ctx):
+        info_descripcion = """
+        Soy un bot desarrollado en python por @tobie y mi misión es aportar comodidad y control a los usuarios!
+        Actualmente estoy en BETA por lo que cualquier feedback es bienvenido!
+        Estos son algunos de mis comandos:
+        """
+        info_general = """
+        !info - Despliega este cartel
+        !cat - Muestra un gato random
+        !dog - Muestra un perro random
+        !say - Decir algo por en canal de voz
+        !vote - Proporner una votación
+        """
+        info_musica = """
+        !play - Reproducir una canción en el canal de voz
+        !skip - Saltear canción actual de la playlist
+        !stop - Termina con la playlist
+        !pause - Pausa la canción
+        !resume - Reanuda la canción
+        !join - Entrar al canal de voz
+        !leave - Salir del canal de voz
+        !playlist - Muestra la playlist actual
+        """
+        embed = discord.Embed(
+            title="Gatito Bot",
+            description=info_descripcion,
+            color=discord.Color.yellow()
+        )
+        embed.add_field(name="General", value=info_general, inline=False)
+        embed.add_field(name="Música", value=info_musica, inline=True)
+        embed.set_footer(text="Aurora Software")
+        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/808020134009897051/1228928657704353873/300566938_376828111300945_5460594727833122005_n.jpg?ex=662dd400&is=661b5f00&hm=cdaa36a6f4b7d672746ba6da260f333dbce6bcc13bfa3ba71e1590deaae5538d&")
+        embed.set_image(url="https://cdn.discordapp.com/attachments/808020134009897051/1228940730215698522/unnamed.gif?ex=662ddf3e&is=661b6a3e&hm=8b85127394e632a5a8a7bc045fe202053e521a34c6a3b3eb75035c0dc6aca50b&")
+
+        await ctx.send(embed=embed)
+
+    @commands.command(help='Muestra un meme de gato')
+    async def cat(self, ctx):
+        response = requests.get('https://api.thecatapi.com/v1/images/search')
+        data = response.json()
+        cat_url = data[0]['url']
+        await ctx.send(cat_url)
+
+    @commands.command(help='Muestra un meme de perro')
+    async def dog(self, ctx):
+        response = requests.get('https://random.dog/woof.json')
+        data = response.json()
+        dog_url = data['url']
+        await ctx.send(dog_url)
+
+    @commands.command(hidden=True, aliases=['w'])
+    async def write(self, ctx, *, arg):
+        member = ctx.message.author
+        await ctx.message.delete()
+        print(f"{member.name} asked to '{ctx.message.content}'")
+        if ctx.message.channel.name == "console":
+            channel = discord.utils.get(member.guild.channels, name="general")
+            await channel.send(arg)
+        else:
+            await ctx.channel.send(arg)
+
+    @commands.command(help='Decir algo por canal de voz')
+    async def say(self, ctx, *, arg):
+        if not ctx.author.voice:
+            await ctx.send("Debes estar en un canal de voz para usar este comando.")
+            return
+        else:
+            channel = ctx.author.voice.channel
+
+        if not ctx.guild.voice_client:
+            await channel.connect()
+        else:
+            await ctx.guild.voice_client.move_to(channel)
+
+        source = 'temp/speech.mp3'
+        language = 'es'
+        sp = gTTS(text = arg, lang = language, slow = False)
+        sp.save(source)
+
+        try:
+            source = discord.FFmpegPCMAudio(source)
+            ctx.guild.voice_client.play(source)
+        except:
+            await ctx.send('Espera, estoy cantando')
+
+    @commands.command(help="Realiza una votacion")                           
+    async def vote(self, ctx, *, message):
+        emb = discord.Embed(title=" VOTACION", description=f"{message}")
+        msg = await ctx.channel.send(embed=emb)
+        await msg.add_reaction('✅')
+        await msg.add_reaction('❌')
+
+    def translate_to_spanish(self, text):
+        url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=" + text
+        response = requests.get(url)
+        if response.status_code == 200:
+            translated_text = response.json()[0][0][0]
+            return translated_text
+        else:
+            return "Error al traducir el texto."
+   
---
+++ music.py
@@ -0,0 +1,151 @@
+import discord
+from discord.ext import commands
+
+import os
+import asyncio
+from pytube import YouTube, Playlist
+from youtube_search import YoutubeSearch
+
+class Music(commands.Cog, name='Musica'):
+    def __init__(self, bot):
+        self.bot = bot
+        self.queue = []
+        self.results = []
+
+    @commands.command(aliases=['p'], help='Reproducir musica en el canal de voz')
+    async def play(self, ctx, *, query):
+        if ctx.message.channel.name != "musica":
+            await ctx.send("Aca no flaco, tenes un canal de música")
+            return
+        if not ctx.author.voice:
+            await ctx.send("Debes estar en un canal de voz para usar este comando.")
+            return
+        else:
+            channel = ctx.author.voice.channel
+
+        if not ctx.guild.voice_client:
+            await channel.connect()
+        else:
+            await ctx.guild.voice_client.move_to(channel)
+
+        if query in ['1','2','3','4','5']:
+            yt = "https://www.youtube.com/watch?v="
+            await self.add_to_queue(ctx, yt + self.results[int(query)-1])
+        elif 'youtube.com/playlist' in query:
+            playlist = Playlist(query)
+            for video_url in playlist.video_urls:
+                self.queue.append(video_url)
+            await ctx.send(f"🎶 Se han añadido **{len(playlist)}** canciones a la playlist")
+            await self.play_song(ctx)
+        elif 'youtube.com' in query:
+            await self.add_to_queue(ctx, query)
+        else:
+            await self.search(ctx, query)
+
+    async def search(self, ctx, query: str):
+        self.results = YoutubeSearch(query, max_results=5).to_dict()
+        videos = [YouTube("https://www.youtube.com" + result['url_suffix']) for result in self.results]
+        self.results = [result['url_suffix'] for result in self.results]
+        if len(videos) < 1:
+            await ctx.send('No hay resultados con ese input')
+        else:
+            searches = ''
+            n = 1
+            for video in videos:
+                searches += f'{n}. 🎶 **{video.author}** - {video.title}\n'
+                n += 1
+            await ctx.send(searches)
+
+    async def add_to_queue(self, ctx, url):
+        self.queue.append(url)
+        await ctx.send(f"🎶 Se ha añadido **{YouTube(url).title}** a la playlist 🔊")
+
+        if len(self.queue) == 1:
+            await self.play_song(ctx)
+
+    async def play_song(self, ctx):
+        url = self.queue[0]
+        try:
+            yt = YouTube(url)
+            stream = yt.streams.filter(only_audio=True).first()
+            output_path = stream.download()
+            _, file = os.path.split(output_path)
+            file = file[:-4]
+
+            try: os.remove('temp/song.mp3')
+            except: pass
+            os.rename(output_path, "temp/song.mp3")
+
+        except Exception as e:
+            print(e)
+            await ctx.send('El video es demasiado largo y Discord no me permite reproducirlo :(')
+            self.queue.pop(0)
+
+        await ctx.send(f"🎶 Reproduciendo **{file}** en **{ctx.author.voice.channel.name}** 🔊")
+        source = discord.FFmpegPCMAudio(source="temp/song.mp3")
+        ctx.guild.voice_client.play(source, after=lambda e: print('terminado', e))
+        while ctx.guild.voice_client.is_playing():
+            await asyncio.sleep(1)
+        self.queue.pop(0)
+        if len(self.queue) > 0:
+            await self.play_song(ctx)
+
+    @commands.command(help='Saltear cancion actual de la playlist')
+    async def skip(self, ctx):
+        if ctx.guild.voice_client and ctx.guild.voice_client.is_playing():
+            ctx.guild.voice_client.stop()
+            await ctx.send("⏭️ Canción omitida. Reproduciendo la siguiente en la cola.")
+        else:
+            await ctx.send("No se está reproduciendo ninguna canción en este momento.")
+
+    @commands.command(help='Mostrar la playlist actual')
+    async def playlist(self, ctx):
+        if len(self.queue) == 0:
+            await ctx.send("La cola de reproducción está vacía.")
+        else:
+            try:
+                queue_list = "\n".join([f"{index + 1}. {YouTube(url).title}" for index, url in enumerate(self.queue)])
+                await ctx.send(f"**Cola de reproducción:**\n{queue_list}")
+            except:
+                await ctx.send(f'La playlist es demasiado larga y no entra en un mensaje x.x')
+
+    @commands.command(help='Terminar la playlist')
+    async def stop(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        self.queue = []
+        voice.stop()
+        await ctx.send('Terminando la reproduccion de la playlist.')
+
+    @commands.command(help='Pausar la musica')
+    async def pause(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        try:
+            voice.pause()
+            await ctx.send('Pausando música ⏸️')
+        except:
+            await ctx.send("No estoy reproduciendo nada.")
+
+    @commands.command(help='Reanuda la musica')
+    async def resume(self, ctx):
+        voice = discord.utils.get(self.bot.voice_clients, guild=ctx.guild)
+        try:
+            voice.resume()
+            await ctx.send('Reanudando música ▶️')
+        except:
+            await ctx.send("No hay una cancion en pausa.")
+
+    @commands.command(help='Salir del canal de voz')
+    async def leave(self, ctx):
+        voice = discord.utils.get(self.voice_clients, guild=ctx.guild)
+        try:
+            if ctx.guild.voice_client.is_playing():
+                await self.stop(ctx)
+            await voice.disconnect()
+        except:
+            await ctx.send("No estoy en un canal de voz.")
+
+    @commands.command(help='Entrar al canal de voz')
+    async def join(self, ctx):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+
